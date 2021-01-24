c68134e5d284e1f4d75ca895ec9032c50cc274f3
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-01-24 01:51:59 -0300

    feat(search-result): colocando title

--- src/pages/SearchResult/index.tsx
+++ src/pages/SearchResult/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import Card from '../../components/Card';
 import Header from '../../components/Header';
@@ -12,6 +12,10 @@ export default function SearchResult() {
       <Header text="Search Results" iconLeft="chevron-left" iconRight="shopping-cart"/>
       <View style={styles.container}>
         <View style={styles.content}>
+          <View style={styles.title}>
+            <Text style={styles.titleText}>Results for “Swift UI”</Text>
+            <Text style={styles.countText}>Total 200</Text>
+          </View>
           <Card title="Declarative interfaces for any Apple Devices" price="IDR 850.000" valueRating="4.5" descriptionRating="By Sarah William" imageLeft={cardRoseImg} />
           <Card title="Declarative interfaces for any Apple Devices" price="IDR 850.000" valueRating="4.5" descriptionRating="By Sarah William" imageLeft={cardRoseImg} />
           <Card title="Declarative interfaces for any Apple Devices" price="IDR 850.000" valueRating="4.5" descriptionRating="By Sarah William" imageLeft={cardRoseImg} />
@@ -35,4 +39,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 25,
     paddingTop: 30
   },
+  title: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  titleText: {
+    fontFamily: 'DMSans_700Bold',
+    color: '#161719',
+    fontSize: 16,
+  },
+  countText: {
+    fontFamily: 'DMSans_700Bold',
+    fontSize: 14,
+    color: '#91919F'
+  }
 });
